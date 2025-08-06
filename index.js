a3e1f2b506c11269e876f84b6c8b1e48be9e0725
gustavo <gustavopolonio1@gmail.com>
2025-08-06 20:48:36 -0300

    feat: create add product popup

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const closeEditPopupButton = document.querySelector(".popup__close");
 const storeName = document.querySelector(".header__title");
 const storeNameInput = document.querySelector(".popup__input_type_name");
 const editStoreForm = document.querySelector("#edit-store-form");
+const addProductButton = document.querySelector(".section__add-btn");
+const addProductPopup = document.querySelector("#add-product-popup");
+const addProductForm = document.querySelector("#add-product-form");
+const productNameInput = document.querySelector(
+  ".popup__input_type_product_name"
+);
+const productURLInput = document.querySelector(".popup__input_type_url");
+const productPriceInput = document.querySelector(".popup__input_type_price");
 
 function handleEscClick(event) {
   console.log(event.key);
@@ -18,16 +26,24 @@ function handleEscClick(event) {
   }
 }
 
-function openPopup() {
-  editPopup.classList.add("popup_is-opened");
+function openPopup(popup) {
+  popup.classList.add("popup_is-opened");
+}
+
+function openEditStorePopup() {
+  openPopup(editPopup);
 
   storeNameInput.value = storeName.textContent;
 
   document.addEventListener("keyup", handleEscClick);
 }
 
-function closePopup() {
-  editPopup.classList.remove("popup_is-opened");
+function openAddProductPopup() {
+  openPopup(addProductPopup);
+}
+
+function closePopup(popup) {
+  popup.classList.remove("popup_is-opened");
 
   document.removeEventListener("keyup", handleEscClick);
 }
@@ -47,19 +63,46 @@ function handleEditStore(event) {
 }
 
 function handleClosePopup(event) {
-  console.log(event.target);
-  console.log(event.target.classList);
-
   if (
     event.target.classList.contains("popup") ||
     event.target.classList.contains("popup__close")
   ) {
-    closePopup();
+    closePopup(event.currentTarget);
   }
 }
 
-editPopupButton.addEventListener("click", openPopup);
+function handleCreateProduct(event) {
+  event.preventDefault();
+
+  const productsContainer = document.querySelector(".products");
+
+  productsContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <div class="product">
+      <strong class="product__title">${productNameInput.value}</strong>
+      <img
+        src=${productURLInput.value}
+        alt=${productNameInput.value}
+        class="product__image"
+      />
+      <div class="product__price-container">
+        <span class="product__price">R$${productPriceInput.value}</span>
+        <button class="product__bookmark-btn"></button>
+      </div>
+    </div>
+    `
+  );
+
+  closePopup(addProductPopup);
+  addProductForm.reset();
+}
+
+editPopupButton.addEventListener("click", openEditStorePopup);
+addProductButton.addEventListener("click", openAddProductPopup);
 
 editStoreForm.addEventListener("submit", handleEditStore);
+addProductForm.addEventListener("submit", handleCreateProduct);
 
 editPopup.addEventListener("click", handleClosePopup);
+addProductPopup.addEventListener("click", handleClosePopup);
