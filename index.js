75b311ed0b0b5c53c5439a011c97238f91098b03
Gustavo Polonio <gustavopolonio1@gmail.com>
2026-01-05 20:59:48 -0300

    refactor: add initial products via JS

--- index.js
+++ index.js
@@ -3,8 +3,49 @@ const editStoreButton = document.querySelector(".header__edit-btn")
 const editStorePopup = document.querySelector("#edit-popup")
 const addProductButton = document.querySelector(".section__add-btn")
 const addProductPopup = document.querySelector("#add-product-popup")
+const productTemplate = document.querySelector("#product-template")
+const productsList = document.querySelector(".products")
 
 // let popupOpened = null
+
+const initialProducts = [
+  {
+    title: "Poster 1",
+    imageUrl: "poster1.png",
+    price: 29.99
+  },
+  {
+    title: "Poster 2",
+    imageUrl: "poster2.png",
+    price: 49.99
+  },
+    {
+    title: "Poster 3",
+    imageUrl: "poster3.png",
+    price: 99.99
+  },
+    {
+    title: "Poster 4",
+    imageUrl: "poster4.png",
+    price: 109.99
+  }
+]
+
+initialProducts.forEach((product) => {
+  const productElement = productTemplate.content.querySelector(".product").cloneNode(true)
+
+  const productTitle = productElement.querySelector(".product__title")
+  productTitle.textContent = product.title
+
+  const productImage = productElement.querySelector(".product__image")
+  productImage.src = `./images/${product.imageUrl}`
+  productImage.alt = product.title
+
+  const productPrice = productElement.querySelector(".product__price")
+  productPrice.textContent = `RS${product.price}`
+
+  productsList.append(productElement)
+})
 
 function openPopup(popup) {
   popup.classList.add("popup_is-opened")
